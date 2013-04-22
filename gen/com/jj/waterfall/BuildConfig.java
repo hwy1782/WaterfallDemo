024aa88033c70e521df46e6ec4d6d608dd5ce3f0
hwy1782 <hwy1782@gmail.com>
2013-04-22 12:50:37 +0800

    test
    fdsaf

---
+++ gen/com/jj/waterfall/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.jj.waterfall;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
